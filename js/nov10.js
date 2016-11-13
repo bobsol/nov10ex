8d0084f1115e83eb176f7d6f7d84fa4eff1658d2
Rob Solomon <bobsol@gmail.com>
2016-11-13 18:06:54 -0500

    add content

---
+++ js/nov10.js
@@ -0,0 +1,15 @@
+/*
+ideas from here: 
+http://stackoverflow.com/questions/19074171/how-to-toggle-a-divs-visibility-by-using-a-button-click
+but didn't end up doing just this...
+*/
+
+var myButton = document.getElementById("last");
+document.getElementById("last").onclick = function(){
+    var gray = document.getElementById("gray");
+    var green = document.getElementById("green");
+    if (gray) {gray.style.display = "block"}
+    if (green) {green.style.display = "block"}
+    document.getElementById("last").innerHTML = "";
+
+};
